f41da3ed373e8b675ff2e672e25983a3d72c91b5
forty <_login_@everteam.org>
2012-09-27 23:14:21 +0200

    Added bootstrap files

--- README.markdown
+++ README.markdown
@@ -1,4 +1,5 @@
-A [giter8][g8] template for [Play2][play2] applications 
+A [giter8][g8] template for [Play2][play2] applications using [Twitter Bootstrap][bootstrap] 
 
 [g8]: http://github.com/n8han/giter8#readme
 [play2]: https://github.com/playframework/Play20/
+[bootstrap]: http://twitter.github.com/bootstrap/
--- src/main/g8/$application_name$/project/Build.scala
+++ src/main/g8/$application_name$/project/Build.scala
@@ -4,14 +4,16 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "$application_name$"
-    val appVersion      = "1.0"
+  val appName = "$application_name$"
+  val appVersion = "1.0"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq( // Add your project dependencies here,
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    lessEntryPoints <<= (sourceDirectory in Compile)(base => (
+      (base / "assets" / "stylesheets" / "bootstrap" / "bootstrap.less") +++
+      (base / "assets" / "stylesheets" / "bootstrap" / "responsive.less")))
       // Add your own project settings here      
     )
 
